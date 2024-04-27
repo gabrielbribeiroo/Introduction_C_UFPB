f737bb926793ac8db803770d757a31d4cd029e9d
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-04-27 16:00:45 -0300

    product stock

---
+++ structs/estoque.c
@@ -0,0 +1,111 @@
+/*
+Faça um programa que leia o código, a descrição, o valor unitário e a quantidade em estoque de 10 produtos comercializados em uma papelaria. 
+Estas informações deverão ser armazenadas em um vetor de estruturas. 
+Depois da leitura dos dados de entrada, o programa deverá: 
+    • Realizar uma rotina que permita alterar a descrição, o valor unitário e a quantidade em estoque de determinado produto, que deverá ser localizado por meio do seu código; 
+    • Realizar uma rotina que mostre todos os produtos cuja descrição comece com determinada letra (informada pelo usuário); 
+    • Mostrar todos os produtos com quantidade em estoque inferior a 5 unidades.
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+#define MAX 3
+
+typedef struct {
+    int codigo, valor, quantidade;
+    char descricao[50];
+}produto;
+
+void LerProduto(produto p[]) {
+    printf("CADASTRO DOS PRODUTOS\n");
+
+    for (int i=0; i<MAX; i++) {
+        printf("\nCodigo do produto: ");
+        scanf("%d%*c", &p[i].codigo);
+
+        printf("Descricao do produto: ");
+        fgets(p[i].descricao, 50, stdin);
+        if (p[i].descricao[strlen(p[i].descricao) - 1] == '\n') {
+            p[i].descricao[strlen(p[i].descricao) - 1] = '\0';
+        }
+
+        printf("Valor do produto: ");
+        scanf("%d%*c", &p[i].valor);
+        printf("Quantidade do produto: ");
+        scanf("%d%*c", &p[i].quantidade);
+    }
+}
+
+void AlterarProduto(produto p[], int codigo) {
+    for (int i=0; i<MAX; i++) {
+        if (p[i].codigo == codigo) {
+            printf("\nAlterando o produto de codigo %d...\n", codigo);
+            printf("Nova descricao: ");
+            // Limpar o buffer antes de ler a nova descricao
+            while (getchar() != '\n'); 
+            fgets(p[i].descricao, 50, stdin); 
+            // Limpar o caractere de nova linha (\n) do buffer
+            if (p[i].descricao[strlen(p[i].descricao) - 1] == '\n') {
+                p[i].descricao[strlen(p[i].descricao) - 1] = '\0';
+            }
+
+            printf("Novo valor: ");
+            scanf("%d%*c", &p[i].valor);
+            printf("Nova quantidade: ");
+            scanf("%d%*c", &p[i].quantidade);
+            printf("Produto alterado com sucesso.\n\n");
+            return;
+        }
+    }
+    printf("Produto nao encontrado.\n\n");
+}
+
+void ProdutosComDescricao(produto p[], char letra) {
+    printf("Produtos com descricao iniciada por '%c':\n", letra);
+    
+    for (int i=0; i<MAX; i++) {
+        if (p[i].descricao[0] == letra) {
+            printf("Codigo: %d\n", p[i].codigo);
+            printf("Descricao: %s\n", p[i].descricao);
+            printf("Valor: %d\n", p[i].valor);
+            printf("Quantidade: %d\n", p[i].quantidade);
+            printf("\n");
+        }
+    }
+}
+
+void ProdutosComEstoqueBaixo(produto p[]) {
+    printf("Produtos com estoque inferior a 5 unidades:\n");
+
+    for (int i=0; i<MAX; i++) {
+        if (p[i].quantidade < 5) {
+            printf("Codigo: %d\n", p[i].codigo);
+            printf("Descricao: %s\n", p[i].descricao);
+            printf("Valor: %d\n", p[i].valor);
+            printf("Quantidade: %d\n", p[i].quantidade);
+            printf("\n");
+        }
+    }
+}
+
+int main() {
+    produto p[MAX];
+    char letra;
+    int codigo;
+
+    LerProduto(p);
+
+    printf("\nDigite o codigo do produto que deseja alterar: ");
+    scanf("%d", &codigo);
+
+    AlterarProduto(p, codigo);
+
+    printf("\nDigite a letra para buscar por descricao: ");
+    scanf(" %c", &letra);
+
+    ProdutosComDescricao(p, letra);
+    ProdutosComEstoqueBaixo(p);
+    
+    return 0;
+}
