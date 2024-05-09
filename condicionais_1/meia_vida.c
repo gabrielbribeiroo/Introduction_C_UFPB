b2ca834bf97bccf6a436e5285d436b1224d0bc01
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-05-09 12:11:10 -0300

    commenting

--- condicionais_1/meia_vida.c
+++ condicionais_1/meia_vida.c
@@ -8,18 +8,21 @@ Escrever a massa inicial, a massa final e o tempo calculado em segundos.
 int main() {
     int tempo=0;
     float massa_inicial, massa_final;
+
     printf("Digite a massa inicial [em gramas]: \n");
-    scanf("%f", &massa_inicial);
-    massa_final = massa_inicial;
+    scanf("%f", &massa_inicial); // Lê o valor da massa inicial
+
+    massa_final = massa_inicial; // Inicializa a massa final recebendo o valor da inicial
 
     while (!(massa_final < 0.5)) {
-        massa_final = massa_final / 2;
-        tempo = tempo + 50;
+        massa_final = massa_final / 2; // A massa final será dividida por 2
+        tempo = tempo + 50; // Soma-se 50 segundos ao tempo
     }
+    
     
     printf("Massa inicial = %.2f gramas. \n"
            "Massa final = %.2f gramas. \n"
-           "Tempo calculado = %d segundos. \n", massa_inicial, massa_final, tempo);
+           "Tempo calculado = %d segundos. \n", massa_inicial, massa_final, tempo); // Mostra os valores encontrados
            
     return 0;
 }
