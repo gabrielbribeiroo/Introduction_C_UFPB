cb2d1af254c16922a89586ca8ef2781b5be6afed
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-05-09 12:12:58 -0300

    commenting

--- condicionais_1/numerador_denominador.c
+++ condicionais_1/numerador_denominador.c
@@ -1,18 +1,20 @@
- /*
-  Faça um programa que calcule e mostre o valor de S
- */
+/*
+Faça um programa que calcule e mostre o valor de S
+*/
 
 #include <stdio.h>
 
 int main() {
-    float numerador=1, denominador=1, soma=0;
+    float numerador=1, denominador=1, soma=0; // Inicializa o numerador e o denominador com 1
+    
+    
     while ((numerador <= 99) && (denominador <= 50)) {
-        soma = soma + (numerador/denominador);
-        numerador = numerador + 2;
-        denominador = denominador + 1;
+        soma = soma + (numerador/denominador); // Soma-se os valores da sequência
+        numerador = numerador + 2; // O numerador aumenta de 2 em 2
+        denominador = denominador + 1; // O denominador aumenta de 1 em 1
     }
 
-    printf("Valor de S = %.2f\n", soma);
+    printf("Valor de S = %.2f\n", soma); // Mostra o valor da soma
     
     return 0;
 }
