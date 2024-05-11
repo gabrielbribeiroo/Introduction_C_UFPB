bdc4f96f64de5ad846036a3d5839f43b5929d93a
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-05-11 13:52:30 -0300

    commenting

--- funções/funcao_caractere.c
+++ funções/funcao_caractere.c
@@ -6,10 +6,9 @@ A função receberá como argumentos o tipo de caractere e o número de linhas q
 #include <stdio.h>
 
 void EscreveLinhas (char c, int L) {
-
     for (int linha = 1; linha <= L; linha++) {
         for (int j = 1; j <= 70; j++) {
-            printf("%c", c);
+            printf("%c", c); // Mostra o mesmo caractere em um determinado número de linhas
         }
         printf("\n");
     }
@@ -20,12 +19,12 @@ int main() {
     char c;
 
     printf("Digite o caractere: \n");
-    c = getchar();
+    c = getchar(); // Lê o caractere que será repetido
     
     printf("Quantas linhas? \n");
-    scanf("%d", &qnt_linhas);
+    scanf("%d", &qnt_linhas); // Lê a quantidade de linhas de repetição
 
-    EscreveLinhas (c, qnt_linhas);
+    EscreveLinhas(c, qnt_linhas); // Chamada de função
 
     return 0;
 }
