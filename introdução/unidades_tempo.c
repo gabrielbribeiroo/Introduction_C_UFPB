8fa811d9349da86e8673080766a40321dbd22eda
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-05-13 15:26:35 -0300

    commenting

--- introdução/unidades_tempo.c
+++ introdução/unidades_tempo.c
@@ -8,14 +8,16 @@ Por exemplo, o número 5984 corresponde a 1 hora 39 minutos e 44 segundos.
 
 int main() {
     int segundos, segundos_sobras, minutos, horas;
-    printf("Digite a quantidade de segundos: \n");
-    scanf("%d", &segundos);
 
+    printf("Digite a quantidade de segundos: \n");
+    scanf("%d", &segundos); // Lê os segundos
+
+    // Transforma as unidades de tempo
     horas = segundos / 3600;
     segundos_sobras = segundos % 3600;
     minutos = segundos_sobras / 60;
     segundos_sobras = segundos_sobras % 60;
 
-    printf("Quantidade de tempo: %d hora(s) e %d minuto(s) e %d segundo(s) \n", horas, minutos, segundos_sobras);
+    printf("Quantidade de tempo: %d hora(s) e %d minuto(s) e %d segundo(s) \n", horas, minutos, segundos_sobras); // Mostra o horário exato
     return 0;
 }
