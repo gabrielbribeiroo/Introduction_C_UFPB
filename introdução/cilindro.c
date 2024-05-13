b14647473aa273e60724cfa6c270b808fbca45de
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-05-13 15:15:09 -0300

    commenting

--- introdução/cilindro.c
+++ introdução/cilindro.c
@@ -12,15 +12,16 @@ int main() {
     float area, volume, raio, altura;
     printf("|----------CILINDRO----------|\n");
     printf("Digite o raio do cilindro: \n");
-    scanf("%f", &raio);
+    scanf("%f", &raio); // Lê o raio
     printf("Digite a altura do cilindro: \n");
-    scanf("%f", &altura);
+    scanf("%f", &altura); // Lê a altura
 
+    // Cálculo de área e volume
     area = 2 * pi * raio * (altura + raio);
     volume = pi * pow(raio,2) * altura;
 
-    printf("Area do cilindro: %.2f\n", area);
-    printf("Volume do cilindro: %.2f\n", volume);
+    printf("Area do cilindro: %.2f\n", area); // Mostra a área
+    printf("Volume do cilindro: %.2f\n", volume); // Mostra o volume
 
     return 0;
 }
