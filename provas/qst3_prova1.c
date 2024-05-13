5c0792b75986c84d5ca4fbdbddeceac6c2b98da6
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-05-13 16:00:21 -0300

    commenting

--- provas/qst3_prova1.c
+++ provas/qst3_prova1.c
@@ -4,17 +4,18 @@ int main() {
     int atual, anterior, qnt_num = 1, soma;
 
     printf("Digite um numero: ");
-    scanf("%d", &atual);
-    soma = atual;
+    scanf("%d", &atual); // Lê um número
+    soma = atual; // Inicializa a soma dos números
 
     do {
-        anterior = atual;
+        anterior = atual; 
         printf("Digite um numero: ");
-        scanf("%d", &atual);
-        soma += atual;
-        qnt_num++;
-    }while ((atual != anterior*2) && (atual != anterior/2));
+        scanf("%d", &atual); // Atualiza o valor do número
+        soma += atual; // Soma os números
+        qnt_num++; // Conta a quantidade de números
+    }while ((atual != anterior*2) && (atual != anterior/2)); // Enquanto o atual for diferente da metade e do dobro do anterior
 
+    // Mostra as informações
     printf("Soma dos numeros = %d\n", soma);
     printf("Quantidade de numeros = %d\n", qnt_num);
     printf("Numeros que finalizaram o programa = %d %d\n", anterior, atual);
