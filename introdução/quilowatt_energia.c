a572fe3cf08bc0b7b4083d35087b6bcb336cf12a
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-05-13 15:21:47 -0300

    commenting

--- introdução/quilowatt_energia.c
+++ introdução/quilowatt_energia.c
@@ -11,14 +11,16 @@ Calcule e mostra:
 
 int main() {
     float salario_minimo, quilowatts, valor;
+
     printf("----------CONSUMO DE QUILOWATTS----------\n");
     printf("Digite o valor do salario minimo atual: \n");
-    scanf("%f", &salario_minimo);
+    scanf("%f", &salario_minimo); // Lê o salário
     printf("Digite o consumo atual de quilowatts: \n");
-    scanf("%f", &quilowatts);
+    scanf("%f", &quilowatts); // Lê o consumo
 
-    valor = quilowatts * 0.2 * salario_minimo;
+    valor = quilowatts * 0.2 * salario_minimo; // Calcula o valor da conta
 
+    // Mostra as informações
     printf("Valor de cada quilowatt: %.2f\n", 0.2*salario_minimo);
     printf("Valor do consumo: %.2f\n", valor);
     printf("Valor a ser pago [DESCONTO DE 15 POR CENTO]: %.2f\n", 0.85*valor);
