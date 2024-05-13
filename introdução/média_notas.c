d0b482e1b87f1b8f8d918b6dcd18531c2ea650ac
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-05-13 15:19:04 -0300

    commenting

--- introdução/média_notas.c
+++ introdução/média_notas.c
@@ -7,17 +7,18 @@ Faça um programa que receba três notas, calcule e mostre a média aritmética 
 
 int main () {
     float nota1, nota2, nota3, media;
-    printf("Digite a primeira nota: \n");
-    scanf("%f", &nota1);//2
     
+    // Leitura das notas
+    printf("Digite a primeira nota: \n");
+    scanf("%f", &nota1);
     printf("Digite a segunda nota: \n");
     scanf("%f", &nota2);
     printf("Digite a terceira nota: \n");
     scanf("%f", &nota3);
 
-    media = (nota1 + nota2 + nota3) / 3;
+    media = (nota1 + nota2 + nota3) / 3; // Cálculo da média
 
-    printf("A media das notas foi: %.2f\n", media);
+    printf("A media das notas foi: %.2f\n", media); // Mostra a média
 
     return 0;
 }
