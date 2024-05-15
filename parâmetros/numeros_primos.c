462d45aa39a638985f68514391d3167e9098ece8
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-05-15 10:15:57 -0300

    commenting

--- parâmetros/numeros_primos.c
+++ parâmetros/numeros_primos.c
@@ -8,21 +8,21 @@ Escrever duas funções, uma que determina se um número é primo e a outra que 
 
 int EhPrimo(int n) {
     if (n <= 1) {
-        return 0;
+        return 0; // O número não é primo
     }
     for (int i=2; i*i<=n; i++) {
         if (n % i == 0) {
-            return 0;
+            return 0; // O número não é primo
         }
     }
-    return 1;
+    return 1; // Número é primo
 } 
 
 int SomaPrimos(int N, int K, int *s) {
     *s = 0;
     for (int i=N; i<=K; i++) {
         if (EhPrimo(i)) {
-            *s += i;
+            *s += i; // Soma os números primos entre dois números 
         }
     }
     return *s;
@@ -32,17 +32,18 @@ int main() {
     int N, K, resultado;
 
     printf("----- CALCULADORA DE PRIMOS -----\n");
+    // Leitura do intervalo
     printf("Digite o valor de N:\n");
     scanf("%d", &N);
     printf("Digite o valor de K:\n");
     scanf("%d", &K);
 
     if ((N <= 0) || (K <= 0)) {
-        printf("Os valores de N e K devem ser positivos. Tente novamente!\n");
+        printf("Os valores de N e K devem ser positivos. Tente novamente!\n"); // Erro
         return 1;
     }
     
-    printf("A soma dos numeros primos entre %d e %d eh: %d\n", N, K, SomaPrimos(N, K, &resultado));
+    printf("A soma dos numeros primos entre %d e %d eh: %d\n", N, K, SomaPrimos(N, K, &resultado)); // Chamada de função
 
     return 0;
 }
