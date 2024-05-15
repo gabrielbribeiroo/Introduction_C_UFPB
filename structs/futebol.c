23b464f426adef42d1bf41b17644ac5fe5fb2b8c
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-05-15 12:26:19 -0300

    commenting

--- structs/futebol.c
+++ structs/futebol.c
@@ -6,16 +6,17 @@ Considerar 10 clubes no total.
 
 #include <stdio.h>
 
-#define MAX 10
+#define MAX 10 // Define uma constante MAX de valor 10 que representa a quantidade de equipes lidas
 
 typedef struct {
     char nome[50];
     int pontos;
-}equipe;
+}equipe; // Declaração de estrutura equipe
 
 void LerEquipe(equipe c[]) {
     printf("CADASTRO DAS EQUIPES\n");
 
+    // Lê as informações das equipes
     for (int i=0; i<MAX; i++) {
         printf("\nNome da equipe: ");
         fgets(c[i].nome, 50, stdin);
@@ -28,23 +29,25 @@ int EquipeVencedora(equipe c[]) {
     int i, index_maior, maior = 0;
 
     for (i=0; i<MAX; i++){
+        // Verifica a maior pontuação
         if (c[i].pontos > maior) {
-        maior = c[i].pontos;
-        index_maior = i;
+            maior = c[i].pontos;
+            index_maior = i;
         }
     }
 
-    return index_maior;
+    return index_maior; // Retorna a posição da equipe vencedora
 }
 
 int main() {
-    equipe c[MAX];
+    equipe c[MAX]; // Array c do tipo equipe (struct)
     int maior;
 
+    // Chamada de funções
     LerEquipe(c);
     maior = EquipeVencedora(c);
     printf("Equipe vencedora = %s, "
-            "Pontos = %d\n", c[maior].nome, c[maior].pontos);
+            "Pontos = %d\n", c[maior].nome, c[maior].pontos); // Mostra a equipe vencedora
 
     return 0;  
 }
