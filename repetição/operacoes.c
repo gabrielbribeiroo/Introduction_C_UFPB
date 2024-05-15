65b9a8c0b832dd3318a6c3b13fdea5d3b883c5ba
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-05-15 10:41:33 -0300

    commenting

--- repetição/operacoes.c
+++ repetição/operacoes.c
@@ -12,23 +12,25 @@ Digite a opção desejada:
 int main() {
     float num1, num2;
     int operacao;
+
     printf("Selecione a opcao desejada: \n");
     printf("[1] Somar dois numeros \n");
     printf("[2] Raiz quadrada de um numero \n");
-    scanf("%d", &operacao);
+    scanf("%d", &operacao); // Lê a operação desejada
 
+    // Calcula a operação, de acordo com a escolhida
     switch(operacao) {
-        case 1:
+        case 1: // Soma
             printf("Digite dois numeros: \n");
             scanf("%f %f", &num1, &num2);
             printf("%.2f + %.2f = %.2f", num1, num2, num1+num2);
             break;
-        case 2:
+        case 2: // Raiz quadrada
             printf("Digite o numero: \n");
             scanf("%f", &num1);
             printf("Raiz Quadrada de %.2f = %.2f", num1, sqrt(num1));
             break;
-        default:
+        default: // Padrão/nenhuma
             printf("OPCAO INVALIDA. TENTE NOVAMENTE!");
     }
     return 0;
