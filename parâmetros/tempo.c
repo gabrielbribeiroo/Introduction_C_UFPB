177f0c0212c28e22cb830fd3177e61799c365f70
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-05-15 10:26:46 -0300

    commenting

--- parâmetros/tempo.c
+++ parâmetros/tempo.c
@@ -6,6 +6,7 @@ para horas, minutos e segundos.
 #include <stdio.h>
 
 void UnidadesDeTempo(int seg, int *sob, int *m, int *h) {
+    // Tranformação para as unidades de tempo
     *h = seg / 3600;
     *sob = seg % 3600;
     *m = *sob / 60;
@@ -15,12 +16,12 @@ void UnidadesDeTempo(int seg, int *sob, int *m, int *h) {
 int main() {
     int segundos, sobras, minutos, horas;
     printf("Digite a quantidade de segundos: \n");
-    scanf("%d", &segundos);
+    scanf("%d", &segundos); // Lê a quantidade de segundos
 
-    UnidadesDeTempo(segundos, &sobras, &minutos, &horas);
+    UnidadesDeTempo(segundos, &sobras, &minutos, &horas); // Chamada de função
 
     printf("Quantidade de tempo:\n"
-           "%d hora(s) e %d minuto(s) e %d segundo(s)\n", horas, minutos, sobras);
+           "%d hora(s) e %d minuto(s) e %d segundo(s)\n", horas, minutos, sobras); // Mostra a quantidade de tempo
 
     return 0;
 }
