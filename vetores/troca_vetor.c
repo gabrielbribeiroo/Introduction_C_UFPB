b39bebd5507b0f62e862fe0630f8a106204c0d3c
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-05-16 15:37:28 -0300

    commenting

--- vetores/troca_vetor.c
+++ vetores/troca_vetor.c
@@ -6,14 +6,14 @@ Escreva ao final o vetor obtido.
 
 #include <stdio.h>
 
-#define TAM 16
+#define TAM 16 // Define uma constante TAM de valor 16 que representa o tamanho do vetor
 
 void LeVetor(int v[], int t) {
     printf("Digite os elementos do vetor:\n");
 
     for (int i=0; i<t; i++) {
         printf("V[%d] = ", i);
-        scanf("%d", &v[i]);
+        scanf("%d", &v[i]); // Leitura de todos os elementos do vetor
     }
 }
 
@@ -21,7 +21,7 @@ void TrocaValores(int v[], int t) {
     int aux;
     for (int i=0; i<t/2; i++) {
         aux = v[i];
-        v[i] = v[t-i-1];
+        v[i] = v[t-i-1]; // Inverte o vetor
         v[t-i-1] = aux;
     }
 }
@@ -30,13 +30,14 @@ void MostraVetor(int v[], int t) {
     printf("\nVetor invertido:\n");
 
     for (int i=0; i<t; i++) {
-        printf("V[%d] = %d\n", i, v[i]);
+        printf("V[%d] = %d\n", i, v[i]); // Mostra todos os elementos do vetor invertido
     }
 }
 
 int main() {
     int vetor[TAM];
 
+    // Chamada das funções
     LeVetor(vetor, TAM);
     TrocaValores(vetor, TAM);
     MostraVetor(vetor, TAM);
