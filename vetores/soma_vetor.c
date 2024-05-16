48826a942d5de1e350475adfdca9c2fad3569101
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-05-16 15:20:06 -0300

    commenting

--- vetores/soma_vetor.c
+++ vetores/soma_vetor.c
@@ -4,14 +4,14 @@ Calcular a soma dos elementos de um vetor de 10 posições.
 
 #include <stdio.h>
 
-#define TAM 10
+#define TAM 10 // Define uma constante TAM de valor 10 que representa o tamanho do vetor
 
 void LeVetor(int v[], int t) {
     printf("Digite os elementos do vetor...\n");
 
     for (int i=0; i<t; i++) {
         printf("V[%d] = ", i);
-        scanf("%d", &v[i]);
+        scanf("%d", &v[i]); // Leitura de todos os elementos do vetor
     }
 }
 
@@ -19,17 +19,17 @@ int SomaVetor(int v[], int t) {
     int i, s = 0;
 
     for (i=0; i<t; i++) {
-        s += v[i];
+        s += v[i]; // Soma todos os elementos do vetor
     }
 
-    return s;
+    return s; // Retorna a soma
 }
 
 int main() {
     int vetor[TAM];
 
+    // Chamada das funções
     LeVetor(vetor, TAM);
-
     printf("\nSoma dos elementos do vetor: %d\n", SomaVetor(vetor, TAM));
 
     return 0;
