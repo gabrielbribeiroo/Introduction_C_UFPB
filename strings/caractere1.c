a6bfc552d98a14ba8bf7237b27c37ffde273d4a8
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-05-16 15:39:54 -0300

    commenting

--- strings/caractere1.c
+++ strings/caractere1.c
@@ -12,24 +12,25 @@ int ocorrencia(const char *str, char C) {
     int i, cont = 0;
 
     for (i = 0; str[i] != '\0'; i++) {
+        // Verifica se o caractere da string é igual ao digitado
         if (str[i] == C) {
-        cont++;
+            cont++; // Conta a quantidade de caracteres iguais ao digitado
         }
     }
 
-    return cont;
+    return cont; // Retorna a quantidade
 }
 
 int main() {
     char letra, frase[50];
 
     printf("Digite uma frase: ");
-    fgets(frase, 50, stdin);
+    fgets(frase, 50, stdin); // Leitura da string
 
     printf("Digite um caractere: ");
-    letra = getchar();
+    letra = getchar(); // Leitura do caractere a ser procurado
 
-    printf("O caractere %c aparece %d vezes na frase %s", letra, ocorrencia(frase, letra), frase);
+    printf("O caractere %c aparece %d vezes na frase %s", letra, ocorrencia(frase, letra), frase); // Chamada da função
     
     return 0;
 }
