0056bfdd8303194fccd29a1b2df5bebd99ef3213
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-05-16 12:26:06 -0300

    commenting

--- vetores/junta_vetores.c
+++ vetores/junta_vetores.c
@@ -8,6 +8,7 @@ Por exemplo, V1 = 0, 1, 2, 3, 4 e V2 = 4, 3, 2, 1, 0, o vetor resultante será R
 void LeVetorA(int a[], int t) {
     printf("Digite os elementos do vetor A:\n");
 
+    // Leitura dos elementos de cada posição do vetor A
     for (int i=0; i<t; i++) {
         printf("A[%d] = ", i);
         scanf("%d", &a[i]);
@@ -17,6 +18,7 @@ void LeVetorA(int a[], int t) {
 void LeVetorB(int b[], int t) {
     printf("\nDigite os elementos do vetor B:\n");
 
+    // Leitura dos elementos de cada posição do vetor B
     for (int i=0; i<t; i++) {
         printf("B[%d] = ", i);
         scanf("%d", &b[i]);
@@ -27,13 +29,13 @@ void ConcatenaVetores(int a[], int b[], int r[], int t1, int t2) {
     int i, j;
     
     for (i=0; i<t1; i++) {
-        r[i] = a[i];
+        r[i] = a[i]; // Atribui-se primeiro os elementos do vetor A
     }
 
     i = 0;
 
     for (j=0; j<t2; j++) {
-        r[t1 + i] = b[j];
+        r[t1 + i] = b[j]; // Depois atribui-se os elementos do vetor B
         i++;
     }
 }
@@ -42,13 +44,14 @@ void MostraVetorR(int r[], int t) {
     printf("\nVetor concatenado:\n");
 
     for (int i=0; i<t; i++) {
-        printf("R[%d] = %d\n", i, r[i]);
+        printf("R[%d] = %d\n", i, r[i]); // Mostra os elementos do vetor concatenado
     }
 }
 
 int main() {
     int tam1, tam2, tamr;
     
+    // Leitura dos tamanhos dos vetores
     printf("Digite o tamanho do vetor A:\n");
     scanf("%d", &tam1);
     printf("Digite o tamanho do vetor B:\n");
@@ -58,6 +61,7 @@ int main() {
     
     int vetorA[tam1], vetorB[tam2], vetorR[tamr];
 
+    // Chamada das funções
     LeVetorA(vetorA, tam1);
     LeVetorB(vetorB, tam2);
     ConcatenaVetores(vetorA, vetorB, vetorR, tam1, tam2);
