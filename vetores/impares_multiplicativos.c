23e6dcc93edb16b49962249948797221714f4671
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-05-16 12:13:26 -0300

    commenting

--- vetores/impares_multiplicativos.c
+++ vetores/impares_multiplicativos.c
@@ -4,12 +4,13 @@ Multiplicar os elementos que estão nos índices ímpares do vetor pela constant
 
 #include <stdio.h>
 
-#define TAM 10
-#define FATOR 15
+#define TAM 10 // Define uma constante TAM de valor 10 que representa o tamanho do vetor
+#define FATOR 15 // Define uma constante FATOR de valor 15 que representa o valor que multiplicará os elementos do vetor
 
 void LeVetor(int v[], int t) {
     printf("Digite os elementos do vetor...\n");
 
+    // Leitura dos elementos de cada posição do vetor
     for (int i=0; i<t; i++) {
         printf("V[%d] = ", i);
         scanf("%d", &v[i]);
@@ -18,7 +19,7 @@ void LeVetor(int v[], int t) {
 
 void MultiplicaPor15(int v[], int t) {
     for (int i=1; i<t; i+=2) {
-        v[i] = v[i] * FATOR;
+        v[i] = v[i] * FATOR; // Multiplica os elementos do vetor por um fator constante determinado
     }
 }
 
@@ -26,7 +27,7 @@ void MostraVetor(int v[], int t) {
     printf("\n\nVetor: ");
 
     for (int i=0; i<t; i++) {
-        printf("%d ", v[i]);
+        printf("%d ", v[i]); // Mostra os elementos do vetor
     }
 
     printf("\n");
@@ -35,6 +36,7 @@ void MostraVetor(int v[], int t) {
 int main() {
     int vetor[TAM];
 
+    // Chamada das funções
     LeVetor(vetor, TAM);
     MultiplicaPor15(vetor, TAM);
     MostraVetor(vetor, TAM);
