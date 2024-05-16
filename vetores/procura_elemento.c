3b4da95af70ff47d1f6230a0385aa9d3816c89d2
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-05-16 15:17:33 -0300

    commenting

--- vetores/procura_elemento.c
+++ vetores/procura_elemento.c
@@ -7,28 +7,27 @@ Se não encontrar, mostrar uma mensagem de que não achou.
 #include <stdlib.h>
 #include <time.h>
 
-#define TAM 10
+#define TAM 10 // Define uma constante TAM de valor 10 que representa o tamanho do vetor
 
 void MostraVetor(int v[], int t) {
     printf("Vetor:\n");
 
     for (int i=0; i<t; i++) {
-        printf("V[%d] = %d\n", i, v[i]);
+        printf("V[%d] = %d\n", i, v[i]); // Mostra todos os elementos do vetor
     }
 }
 
 void ProcurarValor(int v[], int t) {
     int valor, j = 0, total = 0;
-    int *index = malloc(t * sizeof(int));
-
-
+    int *index = malloc(t * sizeof(int)); // Aloca a posição na memória dinâmica
 
     printf("Digite o valor que deseja encontrar:\n");
-    scanf("%d", &valor);
+    scanf("%d", &valor); 
 
     for (int i=0; i<t; i++) {
+        // Verifica se o elemento é igual ao valor digitado
         if (v[i] == valor) {
-            index[j] = i;
+            index[j] = i; // Forma um vetor recebendo os índices dos elementos que são iguais ao valor
             j++;
             total++;
         }
@@ -51,18 +50,19 @@ void ProcurarValor(int v[], int t) {
         }
     }
 
-    free(index);
+    free(index); // Libera a memória
 }  
 
 int main () {
     int vetor[TAM];
 
-    srand((unsigned)time(NULL));
+    srand((unsigned)time(NULL)); 
 
     for (int i = 0; i<TAM; i++) {
-        vetor[i] = 1 + rand()%100;
+        vetor[i] = 1 + rand()%100; // Gera valores aleatórios para o vetor até 100
     }
 
+    // Chamada das funções
     MostraVetor(vetor, TAM);
     ProcurarValor(vetor, TAM);
 
