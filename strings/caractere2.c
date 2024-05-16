3e457b6270bf040ac539d1bebbf12867265e2769
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-05-16 15:44:45 -0300

    commenting

--- strings/caractere2.c
+++ strings/caractere2.c
@@ -6,14 +6,15 @@ Crie um programa que receba uma string e um caractere, e apague todas as ocorrê
 #include <string.h>
 #include <ctype.h>
 
-#define TAM 100
+#define TAM 100 // Define uma constante TAM de valor 100 que representa o tamanho da string
 
 void RemoverCaractere(char *str, char c) {
     int i, j = 0, t = strlen(str);
 
     for (i=0; i<t; i++) {
+        // Verifica se os caracteres da string são diferentes do digitado
         if (str[i] != c) {
-            str[j++] = str[i];
+            str[j++] = str[i]; // Atualiza os elementos da string, removendo o caractere digitado
         }
     }
 
@@ -24,14 +25,13 @@ int main() {
     char letra, frase[TAM];
 
     printf("Digite uma frase: [MAXIMO DE %d CARACTERES]\n", TAM);
-    fgets(frase, sizeof(frase), stdin);
-    frase[strcspn(frase, "\n")] = '\0'; 
+    fgets(frase, sizeof(frase), stdin); // Leitura da string
+    frase[strcspn(frase, "\n")] = '\0'; // Remove o \n, substituindo-o pelo \0, encerrando a string
 
     printf("Digite um caractere:\n");
-    letra = getchar();
+    letra = getchar(); // Leitura do caractere para ser removido
 
-    RemoverCaractere(frase, letra);
-
+    RemoverCaractere(frase, letra); // Chamada da função
     printf("String apos remover o caractere %c: %s\n", letra, frase);
     
     return 0;
