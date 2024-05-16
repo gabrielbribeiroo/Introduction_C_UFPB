0c5aee47f748d48015177944be505b5c641a66d5
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-05-16 12:30:19 -0300

    commenting

--- vetores/media_vetor.c
+++ vetores/media_vetor.c
@@ -4,14 +4,14 @@ Calcular a média dos elementos de um vetor de 10 posições.
 
 #include <stdio.h>
 
-#define TAM 10
+#define TAM 10 // Define uma constante TAM de valor 10 que representa o tamanho do vetor
 
 void LeVetor(int v[], int t) {
     printf("Digite os elementos do vetor...\n");
 
     for (int i=0; i<t; i++) {
         printf("V[%d] = ", i);
-        scanf("%d", &v[i]);
+        scanf("%d", &v[i]); // Leitura dos elementos de cada posição do vetor
     }
 }
 
@@ -19,24 +19,25 @@ int SomaVetor(int v[], int t) {
     int i, s = 0;
 
     for (i=0; i<t; i++) {
-        s += v[i];
+        s += v[i]; // Soma todos os elementos do vetor
     }
 
-    return s;
+    return s; // Retorna a soma
 }
 
 float MediaVetor(int s, int t) {
     float m;
 
-    m = (float) s / t;
+    m = (float) s / t; // Calcula a média dos elementos
 
-    return m;
+    return m; // Retorna a média
 }
 
 int main() {
     int vetor[TAM], soma;
     float media;
 
+    // Chamada das funções
     LeVetor(vetor, TAM);
 
     soma = SomaVetor(vetor, TAM);
