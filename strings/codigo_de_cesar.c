dc796b5d792f27f1d0744e8f938ed08f575ee505
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-05-17 10:42:42 -0300

    commenting

--- strings/codigo_de_cesar.c
+++ strings/codigo_de_cesar.c
@@ -11,38 +11,40 @@ Nova string: D OLJHLUD UDSRVD PDUURP VDOWRX VREUH R FDFKRUUR FDQVDGR
 #include <stdio.h>
 #include <string.h>
 
-#define TAM 100
-#define DESLOCAMENTO 3
+#define TAM 100 // Define uma constante TAM de valor 100 que representa o tamanho da string
+#define DESLOCAMENTO 3 // Define uma constante DESLOCAMENTO de valor 3 que representa quantas letras vão a frente na ASCII para criptografar
 
 void LerFrase(char *frase) {
     printf("Digite uma frase [MAXIMO DE %d caracteres]:\n", TAM-1);
-    fgets(frase, TAM, stdin);
-    frase[strcspn(frase, "\n")] = '\0';
+    fgets(frase, TAM, stdin); // Leitura da string
+    frase[strcspn(frase, "\n")] = '\0'; // Remove o \n, substituindo-o pelo \0, encerrando a string
 }
 
 void CodigoDeCesar(char *frase) {
     int i;
 
     for (i=0; frase[i] != '\0'; i++) {
+        // Verifica se o caractere é maiúsculo ou minúsculo
         if ((frase[i] >= 'A') && (frase[i] <= 'Z')) {
-            frase[i] = ((frase[i] - 'A') + DESLOCAMENTO) % 26 + 'A';
+            frase[i] = ((frase[i] - 'A') + DESLOCAMENTO) % 26 + 'A'; // Incrementa 3 letras a frente 
         }
         else {
             if ((frase[i] >= 'a') && (frase[i] <= 'z')) {
-                frase[i] = ((frase[i] - 'a') + DESLOCAMENTO) % 26 + 'a';
+                frase[i] = ((frase[i] - 'a') + DESLOCAMENTO) % 26 + 'a'; // Incrementa 3 letras a frente
             }
         }       
     }
 }
 
 void MostrarFraseCodificada(char *frase) {
-    printf("Frase codificada: %s\n", frase);
+    printf("Frase codificada: %s\n", frase); // Mostra a frase codificada
 }
 
 
 int main() {
     char frase[TAM];
 
+    // Chamada das funções
     LerFrase(frase);
     CodigoDeCesar(frase);
     MostrarFraseCodificada(frase);
