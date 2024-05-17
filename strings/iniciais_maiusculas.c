42d22cd37d0f350f50343cbf1c9611928718133a
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-05-17 11:30:17 -0300

    commenting

--- strings/iniciais_maiusculas.c
+++ strings/iniciais_maiusculas.c
@@ -10,10 +10,11 @@ void CapitalizarPalavras(char *str) {
     char antes = ' ';
 
     for (i=0; str[i]!='\0'; i++) {
+        // Verifica se o caractere anterior é um espaço e o atual é uma letra minúscula
         if ((antes==' ') && (islower(str[i]))) {
-            str[i] = toupper(str[i]);
+            str[i] = toupper(str[i]); // Transforma o caractere atual para maiúsculo
         }
-        antes = str[i];
+        antes = str[i]; // Atualiza o caractere anterior
     }
 }
 
@@ -21,10 +22,10 @@ int main() {
     char frase[101];
 
     printf("Digite uma frase: [MAXIMO DE 100 CARACTERES]\n");
-    fgets(frase, 101, stdin);
+    fgets(frase, 101, stdin); // Leitura da string
 
-    CapitalizarPalavras(frase);
-    printf("Frase com iniciais maiusculas: %s\n", frase);
+    CapitalizarPalavras(frase); // Chamada da função
+    printf("Frase com iniciais maiusculas: %s\n", frase); // Mostra a frase modificada
 
     return 0;
 }
