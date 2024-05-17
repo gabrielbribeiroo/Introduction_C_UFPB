05ce6eaadcf0f6edc73fc9a3ad089ea1535f3669
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-05-17 11:23:50 -0300

    commenting

--- strings/frase.c
+++ strings/frase.c
@@ -9,21 +9,22 @@ int QuantidadePalavras(const char *str) {
     int i, cont = 1;
 
     for (i=0; str[i]!='\0'; i++) {
+        // Verifica se os caracteres são espaços em branco e se não estão encerradas
         if ((str[i]==' ') && (str[i+1]!=' ') && (str[i+1]!='\0')) {
-            cont++;
+            cont++; // Conta a quantidade de palavras da string
         }
     }
 
-    return cont;
+    return cont; // Retorna a quantidade de palavras
 }
 
 int main() {
     char frase[101];
 
     printf("Digite uma frase: [MAXIMO DE 100 CARACTERES]\n");
-    fgets(frase, 101, stdin);
+    fgets(frase, 101, stdin); // Leitura da string
 
-    printf("A frase %s possui %d palavras.", frase, QuantidadePalavras(frase));
+    printf("A frase %s possui %d palavras.", frase, QuantidadePalavras(frase)); // Chamada da função
     
     return 0;
 }
