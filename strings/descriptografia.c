926668da6b417c2c31d1c3e1e1c9a2a511ed03f3
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-05-17 11:15:15 -0300

    commenting

--- strings/descriptografia.c
+++ strings/descriptografia.c
@@ -11,38 +11,40 @@ Exemplo: Letra = ‘A’; Letra = Letra – 1.
 #include <stdio.h>
 #include <string.h>
 
-#define TAM 101
-#define DESLOCAMENTO -1
+#define TAM 101 // Define uma constante TAM de valor 101 que representa o tamanho da string
+#define DESLOCAMENTO -1 // Define uma constante DESLOCAMENTO de valor -1 que representa quantas letras vão retroceder na ASCII para criptografar
 
 void LerString(char *texto) {
     printf("Digite um texto criptografado [MAXIMO DE %d caracteres]:\n", TAM-1);
-    fgets(texto, TAM, stdin);
-    texto[strcspn(texto, "\n")] = '\0';
+    fgets(texto, TAM, stdin); // Leitura da string
+    texto[strcspn(texto, "\n")] = '\0'; // Remove o \n, substituindo-o pelo \0, encerrando a string
 }
 
 void DescriptografarString(char *texto) {
     int i;
 
     for (i=0; texto[i] != '\0'; i++) {
+        // Verifica se o caractere é maiúsculo ou minúsculo
         if ((texto[i] >= 'A') && (texto[i] <= 'Z')) {
-            texto[i] = ((texto[i] - 'A') + DESLOCAMENTO) % 26 + 'A';
+            texto[i] = ((texto[i] - 'A') + DESLOCAMENTO) % 26 + 'A'; // Retrocede 1 letra
         }
         else {
             if ((texto[i] >= 'a') && (texto[i] <= 'z')) {
-                texto[i] = ((texto[i] - 'a') + DESLOCAMENTO) % 26 + 'a';
+                texto[i] = ((texto[i] - 'a') + DESLOCAMENTO) % 26 + 'a'; // Retrocede 1 letra
             }
         }       
     }
 }
 
 void MostrarStringDescriptografada(char *texto) {
-    printf("Texto descriptografado: %s\n", texto);
+    printf("Texto descriptografado: %s\n", texto); // Mostra o texto criptografado
 }
 
 
 int main() {
     char texto[TAM];
 
+    // Chamada das funções
     LerString(texto);
     DescriptografarString(texto);
     MostrarStringDescriptografada(texto);
