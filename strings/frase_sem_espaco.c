b13944fae3471e0956736460194d14ab16a1d953
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-05-17 11:19:15 -0300

    commenting

--- strings/frase_sem_espaco.c
+++ strings/frase_sem_espaco.c
@@ -6,33 +6,35 @@ Retirar os espaços em branco e depois escrever a string resultante.
 #include <stdio.h>
 #include <string.h>
 
-#define TAM 101
+#define TAM 101 // Define uma constante TAM de valor 101 que representa o tamanho da string
 
 void LerFrase(char *frase) {
     printf("Digite uma frase [MAXIMO DE %d caracteres]:\n", TAM-1);
-    fgets(frase, TAM, stdin);
+    fgets(frase, TAM, stdin); // Leitura da string
 }
 
 void RemoverEspacos(char *frase) {
     int i = 0, j = 0;
     
-    while (frase[i] != '\0') {
+    while (frase[i] != '\0') { // Enquanto a string não for encerrada
+        // Verifica se o caractere é um espaço ou não
         if (frase[i] != ' ') {
-            frase[j] = frase[i];
+            frase[j] = frase[i]; // Copia os caracteres diferentes de espaço
             j++;
         }
         i++;
     }
-    frase[j] = '\0';
+    frase[j] = '\0'; // Encerra a string 
 }
 
 void MostrarFrase(char *frase) {
-    printf("Frase sem espacos em branco: %s\n", frase);
+    printf("Frase sem espacos em branco: %s\n", frase); // Mostra a frase sem espaços
 }
 
 int main() {
     char frase[TAM];
 
+    // Chamada das funções
     LerFrase(frase);
     RemoverEspacos(frase);
     MostrarFrase(frase);
