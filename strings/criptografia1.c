f33c6f1209cd9bf5b27716c378e9b1f9db828f5e
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-05-17 11:07:30 -0300

    commenting

--- strings/criptografia1.c
+++ strings/criptografia1.c
@@ -8,15 +8,15 @@ Saída: ALOCSE AN UOTSE UE
 #include <stdio.h>
 #include <string.h>
 
-#define TAM 101
+#define TAM 101 // Define uma constante TAM de valor 101 que representa o tamanho da string
 
 void InverteFrase(char *str) {
     int i, j;
     char criptografia;
     for (i = 0, j = strlen(str) - 1; i < j; i++, j--) {
-        criptografia = str[i];
-        str[i] = str[j];
-        str[j] = criptografia;
+        criptografia = str[i]; 
+        str[i] = str[j]; // Inverte a ordem dos caracteres
+        str[j] = criptografia; // Inverte a string
     }
 }
 
@@ -24,10 +24,10 @@ int main() {
     char frase[TAM];
 
     printf("Digite uma frase [MAXIMO DE %d caracteres]:\n", TAM - 1);
-    fgets(frase, sizeof(frase), stdin);
+    fgets(frase, sizeof(frase), stdin); // Leitura da string
 
-    InverteFrase(frase);
-    printf("\nFrase criptografada: %s\n", frase);
+    InverteFrase(frase); // Chamada da função
+    printf("\nFrase criptografada: %s\n", frase); // Mostra a frase criptografada
 
     return 0;
 }
