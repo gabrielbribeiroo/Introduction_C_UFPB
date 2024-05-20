effdaf9f8bfbd697c163e67e92070e8f7320cb64
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-05-19 23:40:17 -0300

    commenting

--- strings/maiusculas_ascii.c
+++ strings/maiusculas_ascii.c
@@ -6,29 +6,31 @@ Dica: subtraia 32 dos caracteres, cujo código ASCII está entre 97 e 122.
 #include <stdio.h>
 #include <string.h>
 
-#define TAM 101
+#define TAM 101 // Define uma constante TAM de valor 101 que representa o tamanho da string
 
 void LerFrase(char *frase) {
     printf("Digite uma frase [MAXIMO DE %d caracteres]:\n", TAM-1);
-    fgets(frase, TAM, stdin);
+    fgets(frase, TAM, stdin); // Leitura da string
 }
 
 void MinusculasParaMaiusculas(char *frase) {
     while (*frase != '\0') {
+        // Verifica se o caractere é minúsculo
         if ((*frase >= 'a') && (*frase <= 'z')) {
-            *frase -= 32;
+            *frase -= 32; // Retorna para o valor da letra em maiúsculo (tabela ASCII)
         }
         frase++;
     }
 }
 
 void MostrarFrase(char *frase) {
-    printf("Frase convertida para minusculas: %s\n", frase);
+    printf("Frase convertida para minusculas: %s\n", frase); // Mostra a frase
 }
 
 int main() {
     char frase[TAM];
 
+    // Chamada das funções
     LerFrase(frase);
     MinusculasParaMaiusculas(frase);
     MostrarFrase(frase);
