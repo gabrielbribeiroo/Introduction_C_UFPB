2ba1c3e14ff26dd10096a8bcc545c2edf5e5b6e9
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-05-19 23:37:12 -0300

    commenting

--- strings/login.c
+++ strings/login.c
@@ -11,40 +11,41 @@ Nome: Olívia Silva Santos Marques   >>          Login: OSSMossm
 #include <string.h>
 #include <ctype.h>
 
-#define TAM 101
+#define TAM 101 // Define uma constante TAM de valor 100 que representa o tamanho da string
 
 void GerarLogin(const char *n, char *l) {
     int i, j = 0, t = strlen(n);
 
     for (i=0; i<t; i++) {
         if (((i==0) || (n[i-1] == ' ') || (n[i-1] == '-')) && (isalpha(n[i]))) {
-            l[j++] = toupper(n[i]);
+            l[j++] = toupper(n[i]); // Transforma as letras iniciais em maiúsculas
         }
     }
 
     for (i=0; i<t; i++) {
         if (((i==0) || (n[i-1] == ' ') || (n[i-1] == '-')) && (isalpha(n[i]))) {
-            l[j++] = tolower(n[i]);
+            l[j++] = tolower(n[i]); // Transforma as letras finais em minúsculas
         }
     }
 
-    l[j] = '\0';
+    l[j] = '\0'; // Encerra a string
 }
 
 int main() {
     char nome[TAM], login[TAM];
 
     printf("Nome completo do usuario: [MAXIMO DE %d CARACTERES]\n", TAM-1);
-    fgets(nome, sizeof(nome), stdin);
-    nome[strcspn(nome, "\n")] = '\0';
+    fgets(nome, sizeof(nome), stdin); // Leitura da string
+    nome[strcspn(nome, "\n")] = '\0'; // Substitui o \n da string pelo \0 (encerramento)
 
+    // Verifica o tamanho da string
     if (strlen(nome) > TAM-1) {
         printf("ERRO: O nome fornecido excede o tamanho maximo permitido.\n");
-        return 1;
+        return 1; // Erro
     }
-
+    
+    // Chamada da função
     GerarLogin(nome, login);
-
     printf("Login gerado para o usuario: %s\n", login);
 
     return 0;
