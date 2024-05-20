12a667fdc4398b3a02394e766f442f137f1d75ee
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-05-19 23:32:23 -0300

    commenting

--- strings/string2.c
+++ strings/string2.c
@@ -10,7 +10,7 @@ Use a função toupper() para converter o caracter para maiúsculo.
 #include <time.h>
 #include <ctype.h>
 
-#define TAM 100
+#define TAM 100 // Define uma constante TAM de valor 100 que representa o tamanho da string
 
 void TransformarString(char *str) {
     int i, t = strlen(str), x;
@@ -18,8 +18,8 @@ void TransformarString(char *str) {
     srand(time(NULL));
 
     for (i=0; i<t; i++) {
-        x = rand() % TAM;
-        str[x] = toupper(str[x]);
+        x = rand() % TAM; // Sorteia posições até 100
+        str[x] = toupper(str[x]); // Tranforma os caracteres naquela posição em letra maiúscula
     }
 }
 
@@ -27,11 +27,11 @@ int main() {
     char frase[TAM];
 
     printf("Digite uma frase: [MAXIMO DE %d CARACTERES]\n", TAM);
-    fgets(frase, sizeof(frase), stdin);
-    frase[strcspn(frase, "\n")] = '\0'; 
+    fgets(frase, sizeof(frase), stdin); // Leitura da string
+    frase[strcspn(frase, "\n")] = '\0'; // Substitui o \n da string pelo \0 (encerramento)
 
+    // Chamada das funções
     TransformarString(frase);
-
     printf("String transformada: %s\n", frase);
 
     return 0;
