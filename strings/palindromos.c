900c189bd2cca0069107d5a5207786a9989c3c7a
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-05-21 19:51:21 -0300

    commenting

--- strings/palindromos.c
+++ strings/palindromos.c
@@ -9,20 +9,21 @@ Escreva um programa que ignore os espaços e o caractere ‘-’, ao verificar s
 #include <string.h>
 #include <ctype.h.>
 
-#define TAM 101
+#define TAM 101 // Define uma constante TAM de valor 101 que representa o tamanho da string
 
 void LerFrase(char *frase) {
     printf("Digite uma frase [MAXIMO DE %d caracteres]:\n", TAM-1);
-    fgets(frase, TAM, stdin);
-    frase[strcspn(frase, "\n")] = '\0';
+    fgets(frase, TAM, stdin); // Leitura da string
+    frase[strcspn(frase, "\n")] = '\0'; // Substitui o \n da string pelo \0 (encerramento)
 }
 
 void RemoverEspacos(char *frase) {
     int i = 0, j = 0;
     
     while (frase[i] != '\0') {
+        // Verifica se o caractere é diferente de espaço
         if (frase[i] != ' ') {
-            frase[j] = frase[i];
+            frase[j] = frase[i]; // Mantém apenas os caracteres diferentes de espaços
             j++;
         }
         i++;
@@ -34,8 +35,9 @@ void RemoverPontuacoes(char *frase) {
     int i = 0, j = 0;
     
     while (frase[i] != '\0') {
+        // Verifica se o caractere é alfanumérico ou hífen
         if (isalpha(frase[i]) || frase[i] == '-') {
-            frase[j] = tolower(frase[i]);
+            frase[j] = tolower(frase[i]); // Transforma os caracteres para letras minúsculas
             j++;
         }
         i++;
@@ -47,18 +49,20 @@ int EhPalindromo(char *frase) {
     int i = 0, j = strlen(frase) - 1;
 
     while (i < j) {
+        // Verifica os caracteres do início e do fim
         if (frase[i] != frase[j]) {
-            return 0;
+            return 0; // Não é palíndromo
         }
         i++;
         j--;
     }
-    return 1;
+    return 1; // É palíndromo
 }
 
 void MostrarPalindromo(char *frase) {
-    int comparacao = EhPalindromo(frase);
+    int comparacao = EhPalindromo(frase); // Chamada da função
     
+    // Verifica se é palíndromo
     if (comparacao) {
         printf("Eh palindromo.\n");
     }
@@ -70,6 +74,7 @@ void MostrarPalindromo(char *frase) {
 int main() {
     char frase[TAM];
 
+    // Chamada das funções
     LerFrase(frase);
     RemoverEspacos(frase);
     RemoverPontuacoes(frase);
