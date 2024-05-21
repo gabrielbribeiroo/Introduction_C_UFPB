d96762a107432648f35ba63dde94596b8bab04c1
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-05-21 19:47:10 -0300

    commenting

--- strings/palavras_frase.c
+++ strings/palavras_frase.c
@@ -10,15 +10,16 @@ potentes
 #include <stdio.h>
 #include <string.h>
 
-#define TAM 101
+#define TAM 101 // Define uma constante TAM de valor 101 que representa o tamanho da string
 
 void MostraPalavras(const char *str) {
     int i;
     for (i = 0; str[i] != '\0'; i++) {
-      printf("%c", str[i]);
+      printf("%c", str[i]); // Mostra os caracteres das palavras
 
+      // Verifica se o caractere é um espaço
       if (str[i] == ' ') {
-        printf("\n");
+        printf("\n"); // Pula a linha
       }
     }
 }
@@ -27,9 +28,9 @@ int main() {
     char frase[TAM];
 
     printf("Digite a frase [MAXIMO DE %d caracteres]:\n", TAM - 1);
-    fgets(frase, TAM, stdin);
+    fgets(frase, TAM, stdin); // Leitura da string
     
-    MostraPalavras(frase);
+    MostraPalavras(frase); // Chamada da função
 
     return 0;
 }
