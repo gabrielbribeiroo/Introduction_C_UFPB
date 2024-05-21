ba448bca762912641eba8e1fd4f6f70f1c81beb2
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-05-21 19:41:58 -0300

    commenting

--- strings/palavra_ascii.c
+++ strings/palavra_ascii.c
@@ -6,29 +6,30 @@ Imprima a string resultante.
 #include <stdio.h>
 #include <string.h>
 
-#define TAM 51
+#define TAM 51 // Define uma constante TAM de valor 51 que representa os tamanhos das strings
 
 void LerPalavra(char *palavra) {
     printf("Digite uma palavra [MAXIMO DE %d caracteres]:\n", TAM-1);
-    fgets(palavra, TAM, stdin);
-    palavra[strlen(palavra) - 1] = '\0';
+    fgets(palavra, TAM, stdin); // Leitura da string
+    palavra[strlen(palavra) - 1] = '\0'; // Encerra a string
 }
 
 void SomarASCII(char *palavra) {
     int i;
     
     for (i=0; i < (int)strlen(palavra); i++) {
-        palavra[i] += 1;
+        palavra[i] += 1; // Incrementa 1 unidade do caractere na tabela ASCII
     }
 }
 
 void MostrarPalavra(char *palavra) {
-    printf("Palavra resultante: %s\n", palavra);
+    printf("Palavra resultante: %s\n", palavra); // Mostra a palavra resultante
 }
 
 int main() {
     char palavra[TAM];
 
+    // Chamada das funções
     LerPalavra(palavra);
     SomarASCII(palavra);
     MostrarPalavra(palavra);
